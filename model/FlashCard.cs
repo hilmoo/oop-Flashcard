b877aa9e87d88bbeddc1410a16b06c49549a3a45
hilmoo <hilmusyaf2005@gmail.com>
2024-11-22 15:06:45 +0700

    Add project files.

---
+++ model/FlashCard.cs
@@ -0,0 +1,14 @@
+﻿namespace flashcard.model
+{
+	using System.ComponentModel.DataAnnotations;
+
+	public class FlashCardBasic
+	{
+		[Key]
+		public required int Id { get; set; }
+		public required string Slug { get; set; }
+		public required string Title { get; set; }
+		public required int CurrentProgress { get; set; }
+		public required int TotalProgress { get; set; }
+	}
+}
