94a4501ba3bcbae51cc9e3b8567af27f801316b8
Harun <78070957+runsdev@users.noreply.github.com>
2024-12-16 11:54:12 +0700

    Merge: Auth to master (#2)
    * Add Google auth, refactor DB context, and update utils
    
    * Update environment variables for PostgreSQL configuration and refactor connection string handling
    
    * feat(auth): finalize the google auth and adds integration to supabase
    
    * Refactor and improve code readability
    
    Reformatted code across multiple files for better readability.
    Added null checks in Callback.razor.cs for safer account handling.
    Marked NavigationManager injection as required in Login.razor.cs and Logout.razor.cs.
    Moved logic from Profile.razor to new Profile.razor.cs.
    Changed OnInitializedAsync to OnInitialized in Create.razor.cs.
    Uncommented logging configuration in Program.cs.

---
+++ Components/Pages/Auth/Login.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Components;
+
+namespace flashcard.Components.Pages.Auth
+{
+	public partial class Login : ComponentBase
+	{
+		[CascadingParameter]
+		public required HttpContext HttpContext { get; set; }
+
+		[Inject]
+		public required NavigationManager Navigation { get; set; }
+
+		protected override async Task OnInitializedAsync()
+		{
+			if (HttpContext.User.Identity!.IsAuthenticated)
+			{
+				Console.WriteLine("User is already authenticated");
+				Navigation.NavigateTo("/");
+				return;
+			}
+
+			var authProperties = new AuthenticationProperties
+			{
+				RedirectUri = "/google/callback",
+			};
+			var result = TypedResults.Challenge(authProperties, [GoogleDefaults.AuthenticationScheme]);
+			await result.ExecuteAsync(HttpContext);
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
 using flashcard.Components;
 using flashcard.Data;
-using flashcard.services;
-using Microsoft.AspNetCore.Authentication.Cookies;
+using flashcard.utils;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server;
 using Microsoft.EntityFrameworkCore;
 
 var root = Directory.GetCurrentDirectory();
@@ -10,39 +11,56 @@ DotEnv.Load(dotenv);
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
 
-builder.Services.AddDbContext<FlashcardDbContext>(options =>
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddRazorComponents()
+		.AddInteractiveServerComponents();
+
+builder.Services.AddDbContextFactory<DataContext>(options =>
 {
-	options.UseNpgsql(Environment.GetEnvironmentVariable("DATABASE_URL"));
+	options.UseNpgsql(PostgresConstants.ConnectionString);
 });
 
-//builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-//  .AddCookie(options =>
-//  {
-//    options.Cookie.Name = "auth";
-//    options.LoginPath = "/login";
-//    options.Cookie.MaxAge = TimeSpan.FromDays(1);
-//  });
-//builder.Services.AddAuthorization();
+
 builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddScoped<AuthenticationStateProvider, ServerAuthenticationStateProvider>();
+builder.Services.AddAuthentication("Cookies")
+	.AddCookie(options =>
+	{
+		options.Cookie.Name = AuthenticationConstants.CookieName;
+		options.Cookie.MaxAge = TimeSpan.FromDays(1);
+	}).AddGoogle(googleOpt =>
+	{
+		googleOpt.ClientId = Environment.GetEnvironmentVariable("GOOGLE_CLIENT_ID") ?? throw new InvalidOperationException("GOOGLE_CLIENT_ID is not set in the environment variables.");
+		googleOpt.ClientSecret = Environment.GetEnvironmentVariable("GOOGLE_CLIENT_SECRET") ?? throw new InvalidOperationException("GOOGLE_CLIENT_SECRET is not set in the environment variables.");
+	});
+
+builder.Services.AddSingleton<Supabase.Client>(serviceProvider =>
+{
+	var supabaseUrl = Environment.GetEnvironmentVariable("SUPABASE_URL") ?? throw new InvalidOperationException("SUPABASE_URL not set");
+	var supabaseKey = Environment.GetEnvironmentVariable("SUPABASE_ANON_KEY") ?? throw new InvalidOperationException("SUPABASE_ANON_KEY not set");
+
+	return new Supabase.Client(supabaseUrl, supabaseKey);
+});
 
 builder.Services.AddSingleton<FlashCardService>();
+builder.Services.AddTransient<DbAccountServices>();
 
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+	app.UseExceptionHandler("/Error", createScopeForErrors: true);
 }
 
 app.UseStaticFiles();
 app.UseAntiforgery();
-//app.UseAuthentication();
-//app.UseAuthorization();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+		.AddInteractiveServerRenderMode();
 
 app.Run();
