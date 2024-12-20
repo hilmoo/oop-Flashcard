bf682c0948ffbb40765b486d585093fce41d3716
Harun <78070957+runsdev@users.noreply.github.com>
2024-12-20 08:54:12 +0700

    merge database to Master (#4)
    * Add Google auth, refactor DB context, and update utils
    
    * Update environment variables for PostgreSQL configuration and refactor connection string handling
    
    * feat(auth): finalize the google auth and adds integration to supabase
    
    * feat(db): connect the flashcard to the db (now only read and insert)
    
    * feat(db): implemented all except edit
    
    * feat(state): added localStorage to handle saving flashcard reading state
    
    * feat(visibility): added visibility option
    
    * Refactor flashcard handling and improve code structure
    
    * Add FlashCardService to DI container
    
    * Refactor authentication state handling in Create component
    
    * Simplify AuthenticationStateProvider injection.
    
    * Refactor FlashCard entity to use AccountId instead of GoogleId
    
    * feat(db): finalize the CRUD operations on supabase with EF
    
    * "Refactor deck creation and editing features in FlashCards
    
    * Refactor CSS and update Deck creation UI
    
    * feat(deckmark): finishing CRUD implementation + deckmark
    
    ---------
    
    Co-authored-by: hilmoo <hilmusyaf2005@gmail.com>

---
+++ Migrations/20241220002542_addedDeckMark.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace flashcard.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedDeckMark : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "deckmarks",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    deck_id = table.Column<int>(type: "integer", nullable: false),
+                    account_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_deckmarks", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "deckmarks");
+        }
+    }
+}
--- model/Entities/Flashcard.cs
+++ model/Entities/Flashcard.cs
@@ -3,6 +3,23 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace flashcard.model.Entities
 {
+    [Table("decks")]
+    public record Deck
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        public int Id { get; init; }
+
+        [Required] [Column("slug")] public string? Slug { get; set; }
+        [Required] [Column("title")] public string? Title { get; set; }
+        [Required] [Column("description")] public string? Description { get; set; }
+        [Required] [Column("category")] public string? Category { get; set; }
+        [Required] [Column("total_question")] public int TotalQuestion { get; set; }
+        [Required] [Column("is_public")] public bool IsPublic { get; set; }
+        [Required] [Column("account_id")] public int AccountId { get; set; }
+    }
+
     [Table("flashcards")]
     public record FlashCard
     {
@@ -11,14 +28,20 @@ namespace flashcard.model.Entities
         [Column("id")]
         public int Id { get; init; }
 
-        [Required] [Column("slug")] public string? Slug { get; set; }
+        [Required] [Column("question")] public string? Question { get; set; }
+        [Required] [Column("answer")] public string? Answer { get; set; }
+        [Required] [Column("deck_id")] public int DeckId { get; set; }
+    }
 
-        [Required] [Column("title")] public string? Title { get; set; }
+    [Table("deckmarks")]
+    public record DeckMark
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        public int Id { get; init; }
 
-        [Required] [Column("category")] public string? Category { get; set; }
-
-        [Required] [Column("total_question")] public int TotalQuestion { get; set; }
-
+        [Required] [Column("deck_id")] public int DeckId { get; set; }
         [Required] [Column("account_id")] public int AccountId { get; set; }
     }
 }
